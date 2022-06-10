27ce67b1cd63d1731a75e70fbfea335781269848
jimmykodes <jkeith@kochava.com>
2022-06-10 08:28:56 -0700

    feat: add objects for evaluation

---
+++ object/objects.go
@@ -0,0 +1,37 @@
+package object
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Object interface {
+	Type() Type
+	Inspect() string
+}
+
+type Null struct{}
+
+func (n *Null) Type() Type      { return NullType }
+func (n *Null) Inspect() string { return "null" }
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() Type      { return IntegerType }
+func (i *Integer) Inspect() string { return strconv.FormatInt(i.Value, 10) }
+
+type Float struct {
+	Value float64
+}
+
+func (f *Float) Type() Type      { return FloatType }
+func (f *Float) Inspect() string { return fmt.Sprintf("%f", f.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() Type      { return BoolType }
+func (b *Boolean) Inspect() string { return strconv.FormatBool(b.Value) }
---
+++ object/type.go
@@ -0,0 +1,11 @@
+package object
+
+type Type int
+
+//go:generate stringer -type=Type
+const (
+	NullType Type = iota
+	IntegerType
+	FloatType
+	BoolType
+)
