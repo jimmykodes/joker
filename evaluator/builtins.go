6a9ec1f1ba92d9a0a101e483f00e964dfa4b31cc
jimmykodes <jkeith@kochava.com>
2022-06-14 14:58:45 -0700

    feat: add builtin slice to array and string objects

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -44,4 +44,48 @@ var builtins = map[string]*object.Builtin{
 			return &object.Array{Elements: append(source.Elements, args[1:]...)}
 		},
 	},
+	"slice": {
+		Fn: func(args ...object.Object) object.Object {
+			var (
+				source object.Object
+				start  int64
+				end    int64
+			)
+			switch len(args) {
+			case 0, 1:
+				return newError("invalid number of args, got %d, want 2+", len(args))
+			case 2:
+				source = args[0]
+				if args[1].Type() != object.IntegerType {
+					return newError("cannot slice using type %s, must be %s", args[1].Type(), object.IntegerType)
+				}
+				end = args[1].(*object.Integer).Value
+			case 3:
+				source = args[0]
+				if args[1].Type() != object.IntegerType || args[2].Type() != object.IntegerType {
+					return newError("cannot slice using type %s, must be %s", args[1].Type(), object.IntegerType)
+				}
+				start = args[1].(*object.Integer).Value
+				end = args[2].(*object.Integer).Value
+			}
+			if start < 0 {
+				return newError("starting point of slice cannot be negative")
+			}
+
+			switch src := source.(type) {
+			case *object.Array:
+				if start > int64(len(src.Elements)) || end > int64(len(src.Elements)) {
+					return newError("index out of range [%d] with length %d", end, len(src.Elements))
+				}
+				return &object.Array{Elements: src.Elements[start:end]}
+			case *object.String:
+				if start > int64(len(src.Value)) || end > int64(len(src.Value)) {
+					return newError("index out of range [%d] with length %d", end, len(src.Value))
+				}
+				return &object.String{Value: src.Value[start:end]}
+			default:
+				return newError("invalid source for slice, must be %s or %s", object.ArrayType, object.StringType)
+			}
+		},
+	},
 }
