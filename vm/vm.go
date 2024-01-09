7456842d12982ac5551652202c94cc30368e1d73
jimmykodes <jimmy@khanacademy.org>
2024-01-09 12:08:54 -0800

    feat: add basic vm

---
+++ vm/vm.go
@@ -0,0 +1,58 @@
+package vm
+
+import (
+	"fmt"
+
+	"github.com/jimmykodes/joker/code"
+	"github.com/jimmykodes/joker/compiler"
+	"github.com/jimmykodes/joker/object"
+)
+
+const StackSize = 2048
+
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	stack [StackSize]object.Object
+	sp    int
+}
+
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		constants:    bytecode.Constants,
+		instructions: bytecode.Instructions,
+	}
+}
+
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+		switch op {
+		case code.OpConstant:
+			constIdx := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+			if err := vm.push(vm.constants[constIdx]); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+
+	return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) push(obj object.Object) error {
+	if vm.sp > StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+	vm.stack[vm.sp] = obj
+	vm.sp++
+	return nil
+}
---
+++ vm/vm_test.go
@@ -0,0 +1,71 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jimmykodes/joker/ast"
+	"github.com/jimmykodes/joker/compiler"
+	"github.com/jimmykodes/joker/lexer"
+	"github.com/jimmykodes/joker/object"
+	"github.com/jimmykodes/joker/parser"
+)
+
+type vmTestCase struct {
+	input    string
+	expected any
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 2}, // FIXME
+	}
+	runVmTests(t, tests)
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+		comp := compiler.New()
+		if err := comp.Compile(program); err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+		vm := New(comp.Bytecode())
+		if err := vm.Run(); err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+		stackElem := vm.StackTop()
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+
+func testExpectedObject(t *testing.T, want any, got object.Object) {
+	t.Helper()
+	switch want := want.(type) {
+	case int:
+		if err := testIntegerObject(int64(want), got); err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	}
+}
+
+func testIntegerObject(want int64, got object.Object) error {
+	result, ok := got.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object not an integer. got %T (%v)", got, got)
+	}
+	if result.Value != want {
+		return fmt.Errorf("incorrect value: got %d - want %d", result.Value, want)
+	}
+	return nil
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
